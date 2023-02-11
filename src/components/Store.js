ab5f61e800f286b92c614e0e09eae4ca71676e4f
Your Name <you@example.com>
2023-02-11 13:25:19 +0100

    renetialize the project

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,17 +1,14 @@
 import React, { Fragment } from "react";
 import { Col, Row } from "react-bootstrap";
-import storeItmes from "../data/storeItems.json";
+import storeItems from "../data/storeItems.json";
 import StoreItem from "./StoreItem";
 
 function Store() {
-  const products = [
-    
-  ];
   return (
     <Fragment>
       <h1>Store</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
-        {storeItmes.map((item) => (
+        {storeItems.map((item) => (
           <Col key={item.id}>
             <StoreItem {...item} />
           </Col>
